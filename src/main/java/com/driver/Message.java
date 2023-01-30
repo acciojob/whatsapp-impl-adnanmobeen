a04c2677ac0227b620365bf09305439c4b2fdb96
adnan <adnanmobeen444@gmail.com>
2023-01-30 22:17:54 +0530

    try

--- src/main/java/com/driver/Message.java
+++ src/main/java/com/driver/Message.java
@@ -12,11 +12,11 @@ public class Message {
         this.timestamp = new Date();
     }*/
 
- /*   public Message(int id, String content) {
+    public Message(int id, String content) {
         this.id = id;
         this.content = content;
         this.timestamp = new Date();
-    }*/
+    }
 
     public Message(int id, String content, Date timestamp) {
         this.id = id;
